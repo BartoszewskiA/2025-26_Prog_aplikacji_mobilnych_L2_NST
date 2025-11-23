1bce33e87de2f150eb25a605b0838cbcc1fe4d8f
BartoszewskiA <bartoszewski.a@outlook.com>
2025-11-23 13:32:04 +0100

    23.11.25 v3

--- lab03p01-atlas/app/src/main/java/com/example/lab03p01_atlas/MainActivity.java
+++ lab03p01-atlas/app/src/main/java/com/example/lab03p01_atlas/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.lab03p01_atlas;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +14,18 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tv_nazwa, tv_opis;
+    Button btn_np, btn_ws;
+    ImageView ramka;
+    int[] grafiki = {
+            R.drawable.prawdziwek,
+            R.drawable.podgrzybek,
+            R.drawable.muchomor,
+            R.drawable.kania
+    };
+    String[] nazwy;
+    String[] opisy;
+    int pozycja =0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +36,43 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        tv_nazwa = findViewById(R.id.tv_nazwa);
+        tv_opis = findViewById(R.id.tv_opis);
+        btn_np = findViewById(R.id.btn_np);
+        btn_ws = findViewById(R.id.btn_ws);
+        ramka = findViewById(R.id.ramka);
+        nazwy = getResources().getStringArray(R.array.nazwy);
+        opisy = getResources().getStringArray(R.array.opisy);
+        wyswietl();
+        dodaj_sluchacza();
+    }
+
+    private void dodaj_sluchacza() {
+        btn_np.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pozycja++;
+                if (pozycja >= grafiki.length) {
+                    pozycja = 0;
+                }
+                wyswietl();
+            }
+        });
+        btn_ws.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pozycja--;
+                if (pozycja < 0) {
+                    pozycja = grafiki.length - 1;
+                }
+                wyswietl();
+            }
+        });
+    }
+
+    private void wyswietl() {
+        tv_nazwa.setText(nazwy[pozycja]);
+        tv_opis.setText(opisy[pozycja]);
+        ramka.setImageResource(grafiki[pozycja]);
     }
 }
